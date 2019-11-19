c6ee8e30089965d419d8144a8c7dc2394e6ca922
futeri839 <56431401+futeri839@users.noreply.github.com>
2019-11-20 01:03:55 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <div>
+      <span>since 2019/11/20</span>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import IconLink from "~/components/IconLink.vue";
+
+export default {
+  components: {
+    Logo,
+    IconLink
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
