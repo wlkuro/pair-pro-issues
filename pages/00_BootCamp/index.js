a45e89902b5d5b0aff4486a41aa47db192b8d69c
futeri839 <56431401+futeri839@users.noreply.github.com>
2019-12-06 17:30:53 +0900

    最初はおわり

--- pages/00_BootCamp/index.js
+++ pages/00_BootCamp/index.js
@@ -49,3 +49,18 @@ new Vue({
 		msg: 'input any key'
 	}
 })
+
+Vue.component('todo-item',{
+	props: ['todo'],
+	template: '<li>{{todo.text}}</li>'
+})
+new Vue({
+	el: '#app-7',
+	data: {
+		groceryList: [
+			{ id: 0 , text: 'Vegetables' },
+			{ id: 1 , text: 'Cheese' },
+			{ id: 2 , text: 'Whatever else humans are suppose to eat' }
+		]
+	}
+})
